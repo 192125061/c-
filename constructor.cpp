074b3063eb5aad084adfda08b6ddf7bf08512161
AK.Sathish Kumar <112234185+192125061@users.noreply.github.com>
2022-08-27 13:09:43 +0530

    Add files via upload

---
+++ constructor.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+class bass_class{
+	public:
+		bass_class()
+		{
+		cout<<"Constructor is executed"<<"\n";
+	}
+};
+int main()
+{
+	bass_class ob1;
+	bass_class ob2;
+}
